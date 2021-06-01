b1f053239263793fb88ad2e7c185d31126cc72e2
Eugenijus <augisk@gmail.com>
2021-06-01 21:05:51 +0300

    javascript

--- js/components/Calculator.js
+++ js/components/Calculator.js
@@ -5,7 +5,7 @@ class Calculator {
     this.DOM = null;
     this.buttonsData = [
       { title: 'AC', color: 'white' },
-      { title: '<=|', color: 'white' },
+      { title: '<', color: 'white' },
       { title: '%', color: 'white' },
       { title: '/', color: 'white' },
       { title: '7', color: 'white' },
@@ -25,6 +25,36 @@ class Calculator {
       { title: '=', color: 'orange' },
     ];
     this.init();
+  }
+  render() {
+    const HTML = `<div class="calculator">
+        <div class="screen">
+            <div class="answer">0</div>
+            <div class="equation">2+2+2</div>
+        </div>
+        <div class="keyboard">
+            <div class="button">AC</div>
+            <div class="button">&lt;</div>
+            <div class="button">%</div>
+            <div class="button">/</div>
+            <div class="button">7</div>
+            <div class="button">8</div>
+            <div class="button">9</div>
+            <div class="button">x</div>
+            <div class="button">4</div>
+            <div class="button">5</div>
+            <div class="button">6</div>
+            <div class="button">-</div>
+            <div class="button">1</div>
+            <div class="button">2</div>
+            <div class="button">3</div>
+            <div class="button">+</div>
+            <div class="button">0</div>
+            <div class="button">.</div>
+            <div class="button_ilg">=</div>
+        </div>
+    </div>`;
+    this.DOM.insertAdjacentHTML('beforeend', HTML);
   }
 }
 
